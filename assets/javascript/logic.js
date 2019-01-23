fb2af4b3f6e75c71a0b50d1899777ac019124592
Salman Karim <Lego@macbook-pro-2.home>
2019-01-23 15:23:35 -0600

    update

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,16 +1,35 @@
 $(document).ready(function () {
 
-    
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAJrUhFJXc0D4dGgs8Bx2tmdQWso8qxfGI",
+        authDomain: "train-scheduler-f83d4.firebaseapp.com",
+        databaseURL: "https://train-scheduler-f83d4.firebaseio.com",
+        projectId: "train-scheduler-f83d4",
+        storageBucket: "",
+        messagingSenderId: "349357243992"
+    };
+
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
     $("#button").on("click", function (e) {
         e.preventDefault();
-        
+
         var trainName = $("#trainName").val();
         var destination = $("#destination").val();
+        var frequency = $("#frequency").val()
         var trainTime = $("#trainTime").val();
-        var frequency = $("#frequency")
-        
-        console.log(trainName);
-        console.log(destination);
+
+        $("tbody").append(
+            `<tr>
+            <td>${trainName}</td>
+            <td>${destination}</td>
+            <td>${frequency}</td>
+            <td>${trainTime}</td>
+            <td>test value</td>
+            </tr>`)
     })
 
 });
