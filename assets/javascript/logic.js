14de240afd18e7d084b443c08839e6939c51daca
Salman Karim <Lego@macbook-pro-2.home>
2019-01-24 22:40:24 -0600

    Nearly there

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
         var trainTime = $("#trainTime").val();
 
         var newTrain = {
-            name : trainName,
+            name: trainName,
             destination: destination,
             frequeny: frequency,
             time: trainTime
@@ -31,29 +31,42 @@ $(document).ready(function () {
 
         database.ref().push(newTrain)
 
-        $("#trainName").val(""); 
+        $("#trainName").val("");
         $("#destination").val("");
         $("#frequency").val("")
         $("#trainTime").val("");
     })
 
-    database.ref().on("child_added", function(childsnapshot){
+    database.ref().on("child_added", function (childsnapshot) {
         console.log(childsnapshot.val());
         var trainName = childsnapshot.val().name;
         var destination = childsnapshot.val().destination;
         var frequency = childsnapshot.val().frequeny;
         var trainTime = childsnapshot.val().time;
 
-        
-        
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        var trainTimeConverted = moment(trainTime, "hh:mm").subtract(1, "years");
+        console.log(trainTimeConverted);
+
+        var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        var tRemainder = diffTime % frequency;
+        console.log(tRemainder);
+
+        var minutesAway = frequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + minutesAway);
+
+
         $("tbody").append(
             `<tr>
             <td>${trainName}</td>
             <td>${destination}</td>
             <td>${frequency}</td>
             <td>${trainTime}</td>
-            <td>test value</td>
+            <td>${minutesAway}</td>
             </tr>`)
     })
-
 });
